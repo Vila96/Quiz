3797231b9c58f1264caa3d8ecbbf48b774e1ff5a
Tomas <tomas.vila.sanchez@gmail.com>
2020-10-14 17:50:09 +0200

    Uncomplete version of quiz

---
+++ quiz.js
@@ -0,0 +1,106 @@
+let click = 0;
+let preguntaActual= 0;
+
+let questions = [
+    {question: "¿En que país fue creado el videojuego Tetris?", 
+    answers: ["Estados Unidos", "Reino Unido", "España", "Rusia"],
+    expected: 3
+    },
+    {question: "¿Cual fue la primera consola en el esp" , 
+    answers: ["PS1", "Game Boy", "Game Boy Advance", "N64"],
+    expected: 1
+    },
+    {question: "¿Cual fue el videojuego que tardo mas en desarrollarese?", 
+    answers: ["GTA5", "Far Cry 5", "Duke Nukem Forever", "Red Dead Redemption"],
+    expected: 2
+    },
+    {question: "¿En el primer videojuego de Mario (Donkey Kong), cual fue el nombre original de Mario?", 
+    answers: ["The Plumber", "Jump Man", "The Italian Superman", "Mario Rossi"],
+    expected: 1
+    },
+    {question: "¿Originalmente Assasin's Creed iba a ser un spinoff de que videojuego?", 
+    answers: ["Prince of Persia", "Zelda", "Metal Gear", "Tomb Raider"],
+    expected: 0
+    }
+]
+
+function paint (preguntaPintar) {
+    borrar();
+    let cuestionario = document.querySelector("#onBoard")
+    let form = document.createElement("form");
+    form.id = "formulario";
+    let pregunta = document.createElement("h2");
+    form.appendChild(pregunta);
+    cuestionario.appendChild(form);
+
+    pregunta.innerText = preguntaPintar.question;
+    for (let i = 0; i < preguntaPintar.answers.length; i++ ) {
+        
+    let labelRespuesta = document.createElement("label");
+    labelRespuesta.addEventListener("click", seleccion)
+    let inputRespuesta = document.createElement("input");
+    inputRespuesta.type = "radio"
+    inputRespuesta.name = "respuesta"
+    inputRespuesta.id = `respuesta_${i}`;
+    labelRespuesta.htmlFor = inputRespuesta.id
+        //Boton Siguiente
+        let buttonNext = document.createElement("button")
+        buttonNext.addEventListener ("click", pasarSiguiente)
+        buttonNext.id = "boton"
+    
+
+    labelRespuesta.innerText = preguntaPintar.answers[i];
+    inputRespuesta.value = i;
+    cuestionario.appendChild(buttonNext)
+
+    form.appendChild(labelRespuesta);
+    form.appendChild(inputRespuesta);
+    //Boton
+    
+    }
+}
+
+
+
+paint(questions[preguntaActual]);
+
+
+function borrar () {
+    let form = document.querySelector("#formulario")
+    if (form) {
+        form.remove();
+    }
+}
+
+
+function seleccion (event) {
+    let respuesta = event.target;
+    console.log(respuesta);
+    let respuestaSeleccionada = document.querySelector(`#${respuesta.htmlFor}`).value
+    console.log(respuestaSeleccionada)
+    pasarSiguiente(questions);
+    if (respuestaSeleccionada == questions[preguntaActual].expected) {
+        aciertos++
+    }
+}
+
+function pasarSiguiente (preguntas) {
+ if (preguntaActual < preguntas.length -1) {
+    preguntaActual++;
+    //setTimeout(function() {pintar(preguntas[preguntaActual])}, 2000)
+    paint (preguntas[preguntaActual])
+ }
+ else {
+     borrar();
+ }
+}
+
+function paginaResultados (preguntas) {
+    if (preguntaActual = 3) {
+        window.location.href = "results.html"
+    }
+}
+
+let aciertos = 0;
+let fallos = questions.length - aciertos;
+
