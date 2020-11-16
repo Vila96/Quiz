edac2903f2f00720ae6c555762c3ecef4505f41a
Tomas <tomas.vila.sanchez@gmail.com>
2020-11-16 14:15:49 +0100

    Pre-final version

--- quiz.js
+++ quiz.js
@@ -1,12 +1,14 @@
+//-----------DATOS-----------------
 let click = 0;
 let preguntaActual= 0;
+
 
 let questions = [
     {question: "¿En que país fue creado el videojuego Tetris?", 
     answers: ["Estados Unidos", "Reino Unido", "España", "Rusia"],
     expected: 3
     },
-    {question: "¿Cual fue la primera consola en el esp" , 
+    {question: "¿Cual fue la primera consola en el espacio?" , 
     answers: ["PS1", "Game Boy", "Game Boy Advance", "N64"],
     expected: 1
     },
@@ -23,10 +25,12 @@ let questions = [
     expected: 0
     }
 ]
-
+//-----------FUNCIONES--------------
+// Pintar Preguntas
 function paint (preguntaPintar) {
+    
     borrar();
-    let cuestionario = document.querySelector("#onBoard")
+    let cuestionario = document.querySelector("#onBoard");
     let form = document.createElement("form");
     form.id = "formulario";
     let pregunta = document.createElement("h2");
@@ -36,51 +40,66 @@ function paint (preguntaPintar) {
     pregunta.innerText = preguntaPintar.question;
     for (let i = 0; i < preguntaPintar.answers.length; i++ ) {
         
-    let labelRespuesta = document.createElement("label");
-    labelRespuesta.addEventListener("click", seleccion)
-    let inputRespuesta = document.createElement("input");
-    inputRespuesta.type = "radio"
-    inputRespuesta.name = "respuesta"
-    inputRespuesta.id = `respuesta_${i}`;
-    labelRespuesta.htmlFor = inputRespuesta.id
-        //Boton Siguiente
-        let buttonNext = document.createElement("button")
-        buttonNext.addEventListener ("click", pasarSiguiente)
-        buttonNext.id = "boton"
-    
+        let labelRespuesta = document.createElement("label");
+        labelRespuesta.addEventListener("click", seleccion);
+        let inputRespuesta = document.createElement("input");
+        inputRespuesta.type = "radio";
+        inputRespuesta.name = "respuesta";
+        inputRespuesta.id = `respuesta_${i}`;
+        labelRespuesta.htmlFor = inputRespuesta.id;
 
-    labelRespuesta.innerText = preguntaPintar.answers[i];
-    inputRespuesta.value = i;
-    cuestionario.appendChild(buttonNext)
+        labelRespuesta.innerText = preguntaPintar.answers[i];
+        inputRespuesta.value = i;
 
-    form.appendChild(labelRespuesta);
-    form.appendChild(inputRespuesta);
-    //Boton
+        form.appendChild(labelRespuesta);
+        form.appendChild(inputRespuesta);
+        //-------------AQUI PARA PAG RESULTADO?
     
     }
+            //Boton Next
+            let buttonNext = document.createElement("button");
+            buttonNext.addEventListener ("click", () => pasarSiguiente(questions));
+            buttonNext.id = "botonSiguiente";
+            buttonNext.innerText = "Siguiente";
+            cuestionario.appendChild(buttonNext);
+            //Boton Anterior
+            let buttonPrevious = document.createElement("button");
+            buttonPrevious.addEventListener ("click", () => pasarAnterior(questions));
+            buttonPrevious.id = "botonAnterior";
+            buttonPrevious.innerText = "Anterior";
+            cuestionario.appendChild(buttonPrevious);
+        
 }
-
-
 
 paint(questions[preguntaActual]);
 
 
-function borrar () {
-    let form = document.querySelector("#formulario")
-    if (form) {
-        form.remove();
-    }
-}
-
-
+//Seleccion de pregunta
 function seleccion (event) {
+
     let respuesta = event.target;
     console.log(respuesta);
-    let respuestaSeleccionada = document.querySelector(`#${respuesta.htmlFor}`).value
-    console.log(respuestaSeleccionada)
+    let respuestaSeleccionada = document.querySelector(`#${respuesta.htmlFor}`).value;
+    console.log(respuestaSeleccionada);
     pasarSiguiente(questions);
-    if (respuestaSeleccionada == questions[preguntaActual].expected) {
-        aciertos++
+    if (respuestaSeleccionada = questions[preguntaActual].expected) {
+        respuesta.style.color = "green";
+        aciertos++;
+    }
+
+    //Mostrar el resultado en pagina resultado
+    //document.getElementById("results").innerHTML = `${aciertos} out of ${questions.length}`
+}
+
+// Borrar formulario
+function borrar () {
+    let form = document.querySelector("#formulario");
+    let buttonNext = document.querySelector("#botonSiguiente");
+    let buttonPrevious = document.querySelector("#botonAnterior")
+    if (form, buttonNext, buttonPrevious) {
+        form.remove();
+        buttonNext.remove();
+        buttonPrevious.remove();
     }
 }
 
@@ -88,18 +107,34 @@ function pasarSiguiente (preguntas) {
  if (preguntaActual < preguntas.length -1) {
     preguntaActual++;
     //setTimeout(function() {pintar(preguntas[preguntaActual])}, 2000)
-    paint (preguntas[preguntaActual])
+    paint (preguntas[preguntaActual]);
+    console.log(preguntaActual)
+
+    
+   
  }
  else {
      borrar();
+                  //Results
+                    alert(`${aciertos} out of ${questions.length}`)
  }
+
+ 
 }
 
-function paginaResultados (preguntas) {
-    if (preguntaActual = 3) {
-        window.location.href = "results.html"
-    }
+//Funcion para Boton anterior
+function pasarAnterior (preguntas){
+    if (preguntaActual < preguntas.length) {
+        preguntaActual--;
+        //setTimeout(function() {pintar(preguntas[preguntaActual])}, 2000)
+        paint (preguntas[preguntaActual]);
+     }
+     else {
+         borrar();
+     }
 }
+
+
 
 let aciertos = 0;
 let fallos = questions.length - aciertos;
